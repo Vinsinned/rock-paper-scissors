07eebf7d00519a17e0921e2e5e6b8ddf0c8df669
vinson <dagger1245653@gmail.com>
2021-06-02 11:16:25 -0700

    added scores and also fixed the outcome bug where the text doesn't change like I want it to

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+//creates two variables that keeps track of the player's scores
 let score = 0;
 let computerScore = 0;
-const buttons = document.querySelectorAll('button');
-const roundResults = document.querySelector('#results');
+//creates a variable called playerSelection which will store rock, paper, or scissor
+let playerSelection = "";
+//selects all buttons and stores it into the "buttons" variable
+const buttons = document.querySelectorAll("button");
+//gets the results div and stores it into roundResults
+const roundResults = document.querySelector("#results");
+//gets div of both boxes so the scores can be appended
+const box1 = document.querySelector("#box1");
+const box2 = document.querySelector("#box2");
+//creates a paragraph to display the outcome
+const winnerDeclaration = document.createElement("p");
+//creates a paragraph to display the player's score
+const displayScore = document.createElement("p");
+//creates a paragraph to display the computer's score
+const displayComputerScore = document.createElement("p");
+
 //Computer chooses rock, paper, or scissors randomly
 function computerPlay() {
     let random = Math.floor(Math.random()*3) + 1;
@@ -15,59 +30,62 @@ function computerPlay() {
         return ("ERROR, PLEASE DEBUG YOUR CODE");
     }
 }
-function deleteChild() {
-    
-}
 //check if the player wins or loses and how
 function playGame(playerSelection, computerSelection) {
     if (playerSelection == "rock" && computerSelection == "scissors") {
 
         score += 1;
-        const rockScissors = document.createElement('p');
-        rockScissors.textContent = "You won! Rock defeats scissors";
-        roundResults.appendChild(rockScissors);
+        winnerDeclaration.textContent = "You won! Rock defeats Scissors";
+        displayScore.textContent = score;
+        displayComputerScore.textContent = computerScore;
         return;
 
     } else if (playerSelection == "rock" && computerSelection == "paper") {
 
         computerScore += 1;
-        const rockPaper = document.createElement('p');
-        rockPaper.textContent = "You lost! Rock loses to paper";
-        return roundResults.appendChild(rockPaper);
+        winnerDeclaration.textContent = "You lost! Rock loses to Paper";
+        displayScore.textContent = score;
+        displayComputerScore.textContent = computerScore;
+        return;
 
     } else if (playerSelection == "paper" && computerSelection == "rock") {
 
         score += 1;
-        const paperRock = document.createElement('p');
-        paperRock.textContent = "You won! Paper defeats rock";
-        return roundResults.appendChild(rockPaper);
+        winnerDeclaration.textContent = "You won! Paper defeats rock";
+        displayScore.textContent = score;
+        displayComputerScore.textContent = computerScore;
+        return;
 
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
 
         computerScore += 1;
-        const paperScissor = document.createElement('p');
-        paperScissor.textContent = "You lost! Paper loses to scissors";
-        return roundResults.appendChild(rockScissor);
+        winnerDeclaration.textContent = "You lost! Paper loses to scissors";
+        displayScore.textContent = score;
+        displayComputerScore.textContent = computerScore;
+        return;
 
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
 
         score += 1;
-        const scissorPaper = document.createElement('p');
-        scissorPaper.textContent = "You won! Scissors defeats paper";
-        return roundResults.appendChild(scissorPaper);
+        winnerDeclaration.textContent = "You won! Scissors defeats paper";
+        displayScore.textContent = score;
+        displayComputerScore.textContent = computerScore;
+        return;
 
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
 
         computerScore += 1;
-        const scissorRock = document.createElement('p');
-        scissorRock.textContent = "You lost! Scissors losses to rock";
-        return roundResults.appendChild(scissorRock);
+        winnerDeclaration.textContent = "You lost! Scissors loses to rock";
+        displayScore.textContent = score;
+        displayComputerScore.textContent = computerScore;
+        return;
 
     } else {
 
-        const tie = document.createElement('p');
-        tie.textContent = "It's a tie!";
-        return roundResults.appendChild(tie);
+        winnerDeclaration.textContent = "It's a tie!";
+        displayScore.textContent = score;
+        displayComputerScore.textContent = computerScore;
+        return;
 
     }
 }
@@ -85,7 +103,6 @@ function transferId() {
 }
 
 //Once the player chooses a button, it calls back the other functions to run.
-let playerSelection = '';
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
             playerSelection = button.id;
@@ -93,3 +110,8 @@ let playerSelection = '';
             playGame(transferId(), computerPlay());
         });
     });
+//adds the paragraph created earlier as a child of the results div
+roundResults.appendChild(winnerDeclaration);
+//appends the scores as a paragraph
+box1.appendChild(displayScore);
+box2.appendChild(displayComputerScore);
